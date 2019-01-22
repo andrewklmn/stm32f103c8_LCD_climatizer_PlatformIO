069be081d6b8e447bc5fa12b8df1aa446abf0824
andrewklmn <andrew.klmn@gmail.com>
2019-01-22 09:44:02 +0200

    temp correction -3

--- src/stm32f103c8t6_arduino_LCD_wifi_climatizer.cpp
+++ src/stm32f103c8t6_arduino_LCD_wifi_climatizer.cpp
@@ -115,7 +115,7 @@ void loop() {
           humidity=0;
           //return;
         } else {
-          temperature = temperature-2;
+          temperature = temperature-3;
         };
 
         screen1.setCursor(5,0);
