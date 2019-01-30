8b29dd9d1a9fd765eddf9385e59abdd4d34bb927
andrewklmn <andrew.klmn@gmail.com>
2019-01-30 12:59:31 +0200

    RED 1.1

--- src/stm32f103c8t6_arduino_LCD_wifi_climatizer.cpp
+++ src/stm32f103c8t6_arduino_LCD_wifi_climatizer.cpp
@@ -36,17 +36,18 @@ On_off_driver water(5);
 byte temperature = 0;
 byte humidity = 0;
 
+/*
 // FOR GREEN ONE - small
 const byte target_temp = 20;
 const byte comfort_temp = 19;
 const byte target_humidity = 50;
+*/
 
-/*
 // FOR RED ONE - big
 const byte target_temp = 19;
 const byte comfort_temp = 18;
 const byte target_humidity = 50;
-*/
+
 
 int pass_adc_reading_cycles = 10;
 int err = SimpleDHTErrSuccess;
@@ -54,14 +55,14 @@ int err = SimpleDHTErrSuccess;
 int monitor_mode = 0;
 int sensorValue = 0;
 
-LiquidCrystal_I2C  screen1(0x27,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified backpack GREEN
-//LiquidCrystal_I2C  screen1(0x3F,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified backpack RED
+//LiquidCrystal_I2C  screen1(0x27,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified backpack GREEN
+LiquidCrystal_I2C  screen1(0x3F,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified backpack RED
 
 int MQ135_ao_from_adc_to_ppm(int ADC_value, int temp_value, int humidity_value) {
 
 
 
-
+/*
   // FOR GREEN ONE
   #define RLOAD 1000
   /// Calibration resistance at atmospheric CO2 level
@@ -69,9 +70,9 @@ int MQ135_ao_from_adc_to_ppm(int ADC_value, int temp_value, int humidity_value) 
   /// Parameters for calculating ppm of CO2 from sensor resistance
   #define PARA 76.
   #define PARB 1.9
+*/
 
 
-  /*
     // FOR RED ONE
     #define RLOAD 1000
     /// Calibration resistance at atmospheric CO2 level
@@ -79,7 +80,6 @@ int MQ135_ao_from_adc_to_ppm(int ADC_value, int temp_value, int humidity_value) 
     /// Parameters for calculating ppm of CO2 from sensor resistance
     #define PARA 116
     #define PARB 2.8
-    */
 
 
     float Up = 5.0;
